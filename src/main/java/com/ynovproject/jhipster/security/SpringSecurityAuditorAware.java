edd1f01fd197216a51244eea04cd5984a6c7ada5
vexs <coriane.leig@gmail.com>
2022-10-13 13:07:38 +0200

    initial commit

---
+++ src/main/java/com/ynovproject/jhipster/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.ynovproject.jhipster.security;
+
+import com.ynovproject.jhipster.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ src/test/java/com/ynovproject/jhipster/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.ynovproject.jhipster;
+
+import com.ynovproject.jhipster.YnovprojectApp;
+import com.ynovproject.jhipster.config.AsyncSyncConfiguration;
+import com.ynovproject.jhipster.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { YnovprojectApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
